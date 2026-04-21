230a9ab7914dbedb5cda976ee23085beebcdcbb7
loayashraf2288-boop <loay.ashraf2288@gmail.com>
2026-04-21 17:58:18 +0200

    Create index.js for Home page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to TechStore!</h1>
+            <p>Explore our categories and products.</p>
+            {/* Add banner, categories, products components here */}
+        </div>
+    );
+};
+
+export default Home;
